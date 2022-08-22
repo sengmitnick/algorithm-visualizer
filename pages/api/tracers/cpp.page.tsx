c435d2ff7205f167b699c0005843496b18ef583c
sengmitnick <sengmitnick@163.com>
2022-08-22 06:39:01 +0000

    optimize cpp

--- pages/api/tracers/cpp.page.tsx
+++ pages/api/tracers/cpp.page.tsx
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
 import path from "path";
+import { v4 } from "uuid";
 import { promisify } from "util";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { errorToJSON } from "utils";
@@ -11,16 +12,17 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const dir = path.resolve("tmp");
+    const dir = path.resolve("tmp", v4().replaceAll('-', ''));
     await dirExists(dir);
     await promisify(fs.writeFile)(path.join(dir, "main.cpp"), req.body.code);
     await execute(
-      `cd ${dir} && g++ main.cpp -o Main -O2 -std=c++11 -lcurl -B "/var/empty/local" && ALGORITHM_VISUALIZER=1 ./Main`
+      `cd ${dir} && g++ main.cpp -o Main -O2 -std=c++11 -lcurl && ALGORITHM_VISUALIZER=1 ./Main`
     );
     const commands = await promisify(fs.readFile)(
       path.join(dir, "visualization.json"),
       { encoding: "utf-8" }
     );
+    await promisify(fs.rmdir)(dir)
     res.status(200).json(JSON.parse(commands));
   } catch (error: any) {
     res.status(404).json({ error: errorToJSON(error) });
