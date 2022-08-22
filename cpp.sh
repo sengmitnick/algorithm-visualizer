aa67f0eb785fabe017d81067b9cbbebced5b0e4d
sengmitnick <sengmitnick@163.com>
2022-08-22 06:39:26 +0000

    add cpp in gitpod init

--- cpp.sh
+++ cpp.sh
@@ -1,10 +1,12 @@
+sudo chmod -R 777 /usr/local
 curl --create-dirs -o /usr/local/include/nlohmann/json.hpp -L "https://github.com/nlohmann/json/releases/download/v3.1.2/json.hpp"
-curl --create-dirs -o /usr/tmp/algorithm-visualizer.tar.gz -L "https://github.com/algorithm-visualizer/tracers.cpp/archive/v2.3.6.tar.gz"
-cd /usr/tmp
+curl --create-dirs -o /workspace/tmp/algorithm-visualizer.tar.gz -L "https://github.com/algorithm-visualizer/tracers.cpp/archive/v2.3.6.tar.gz"
+sudo chmod -R 777 /usr/local
+cd /workspace/tmp
 mkdir algorithm-visualizer
 tar xvzf algorithm-visualizer.tar.gz -C algorithm-visualizer --strip-components=1
-cd /usr/tmp/algorithm-visualizer
+cd /workspace/tmp/algorithm-visualizer
 mkdir build
 cd build
 cmake ..
-make install
+sudo make install
